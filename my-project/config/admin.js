9b0a7122d8848545c5a4a8f2dfd2b4b8adad3b28
ashutoshtanthetaa <121015617+ashutoshtanthetaa@users.noreply.github.com>
2023-05-22 15:06:27 +0530

    changes done

--- my-project/config/admin.js
+++ my-project/config/admin.js
@@ -1,13 +1,17 @@
 module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env("API_TOKEN_SALT", "someRandomLongString"),
+  },
+  auditLogs: {
+    // only accessible with an Enterprise plan
+    enabled: env.bool("AUDIT_LOGS_ENABLED", true),
+  },
+  auth: {
+    secret: env("ADMIN_JWT_SECRET", "someSecretKey"),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env("TRANSFER_TOKEN_SALT", "anotherRandomLongString"),
     },
   },
 });
